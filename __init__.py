5b427babd0f3d682cc663855cd55338b1dc5ae2d
modulus <thejohnskauge@gmail.com>
2013-11-06 15:46:34 +0100

    New try

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
+from flask import app
+import os
+import sys
+
 __author__ = 'John'
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
+
+    root = logging.getLogger()
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
