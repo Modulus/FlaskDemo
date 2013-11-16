d4198cbb3130530970e3f6dc61032505c800a46e
modulus <thejohnskauge@gmail.com>
2013-11-16 13:49:21 +0100

    Converted app to Mongoengine

--- flask_app.py
+++ flask_app.py
@@ -1,155 +1,50 @@
-#coding : utf-8
-from datetime import datetime
+import urlparse
+from flask.ext.mongoengine import MongoEngine
 import os
-import logging
-import sys
-from bson import ObjectId
-from flask import Flask, request, jsonify
-from flask.ext.pymongo import PyMongo
-from models.message import Message
-import bcrypt
-from models.user import User
+from resources.message_resource import MessageResource
+from resources.users_resource import UsersResource
 
-#MongoHQ url on heroku
-MONGO_URL = os.environ.get('MONGOHQ_URL')
-
-app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "demo"
-if MONGO_URL:
-    app.config["MONGO_URI"] = MONGO_URL
-
-    #Configure logger for heroku
-
-    logging.basicConfig(stream=sys.stdout, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                        level=logging.DEBUG)
-
-mongo = PyMongo(app, config_prefix="MONGO")
-
-#Same as client = MongoClient('localhost', 27017)
-#Or client = MongoClient('mongodb://localhost:27017/')
-
-@app.route('/')
-def helloWorld():
-    return jsonify({"header": "Welcome to the demo!", "message":
-                    "This will show simple usage of Python Flask and Pymongo"})
+__author__ = 'modulus'
+from flask import Flask
+from flask.ext import restful
+from resources.messages_resource import MessagesResource
+from resources.user_resource import UserResource
 
 
-@app.route("/user/<user_id>", methods=["GET", "PUT", "DELETE"])
-def handleUser(user_id):
-    app.logger.debug("Handling user")
+class FlaskApp(object):
 
-    if request.method == "PUT":
-        return "Put called on user with id "+user_id
+    def __init__(self):
+        MONGOHQ_URL = os.environ.get('MONGOHQ_URL')
 
-    elif request.method == "DELETE":
-        user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
-        if user is not None:
-            app.logger.debug("Delete called on user with id [{userid}]".format(userid=user["_id"]))
-            mongo.db.users.remove({"_id": ObjectId(user["_id"])})
-            user["_id"] = str(user["_id"])
-            return jsonify(user)
+        self.app = Flask(__name__)
+
+        self.app.config["MONGO_DBNAME"] = "demo"
+        if MONGOHQ_URL:
+            url = urlparse.urlparse(MONGOHQ_URL)
+            self.app.config.setdefault('MONGODB_USERNAME', url.username)
+            self.app.config.setdefault('MONGODB_PASSWORD', url.password)
+            self.app.config.setdefault('MONGODB_HOST', url.hostname)
+            self.app.config.setdefault('MONGODB_PORT', url.port)
+            self.app.config.setdefault('MONGODB_DB', url.path[1:])
+
         else:
-            return jsonify({"Message": "Non existing user"})
+            self.app.config["MONGODB_HOST"] = "localhost"
+            self.app.config["MONGODB_PORT"] = "27017"
+            self.app.config["MONGODB_DB"] = "demo"
 
-    elif request.method == "GET":
-        existingUser = mongo.db.users.find_one({"_id": ObjectId(user_id)})
-        if existingUser is not None:
-            existingUser["_id"] = str(existingUser["_id"])
-            return jsonify(existingUser)
-        else:
-            return jsonify({"Message": "Non existing user"})
+        self.db = MongoEngine(self.app)
 
-@app.route("/users", methods=["GET"])
-def getAllUsers():
-    users = []
-    logger = logging.getLogger("UserLogger")
-    logger.debug("Getting users")
-    for user in mongo.db.users.find():
-        logger.debug(user)
-        user["_id"] = str(user["_id"])
-        users.append(user)
+        self.api = restful.Api(self.app)
+        self.api.add_resource(UserResource, "/user/<string:id>", "/user", "/user/")
+        self.api.add_resource(UsersResource, "/", "/users", "/user/<string:user_id>/friends",
+                              "/user/<string:user_id>/friends/")
+        self.api.add_resource(MessageResource, "/message/<string:message_id>", "/message/<string:message_id>/")
+        self.api.add_resource(MessagesResource, "/messages/<string:user_id>", "/messages/<string:user_id>/")
 
-    if not user:
-        logger.error("No users found")
-
-    return jsonify(users=users)
+    def run(self, debug=False):
+        self.app.run(debug=debug)
 
 
-@app.route("/user", methods=["POST"])
-def addUser():
-    logger = logging.getLogger()
-    logger.debug("Adding user")
-    userName = request.form["username"]
-    firstName = request.form["firstname"]
-    lastName = request.form["lastname"]
-    password = request.form["password"]
-    created = datetime.now()
-    logger.debug("Hashing")
-    hash = bcrypt.hashpw(password, bcrypt.gensalt())
-
-    logger.debug("Hashing finished")
-    logger.debug("Trying to find existing user")
-    existingUser = mongo.db.users.find_one({"userName": userName})
-    if existingUser:
-        logger.debug(existingUser["userName"])
-    logger.debug("After user")
-
-    if existingUser is None:
-        user = User(userName=userName, firstName=firstName, lastName=lastName, password=hash, created=created)
-        id = mongo.db.users.insert(user.json())
-        user._id = id
-        return jsonify(user.json())
-    else:
-        logger.debug("User allready exists")
-        return jsonify({"message" : "User allready exists"})
-
-
-@app.route("/user/<user_id>/messages", methods=["GET"])
-def getAllMessages(user_id):
-    app.logger.debug("Fetching messages for user_id"+user_id)
-    messages = []
-
-    #Compact way of query
-    for message in mongo.db.messages.find({"$or": [{"sender": ObjectId(user_id)}, {"receiver": ObjectId(user_id)}]}):
-        message["_id"] = str(message["_id"])
-        message["sender"] = str(message["sender"])
-        message["receiver"] = str(message["receiver"])
-        messages.append(message)
-
-    return jsonify(results=messages)
-
-@app.route("/user/<user_id>/message/<message_id>", methods=["GET"])
-def getMessage(user_id, message_id):
-    message = mongo.db.messages.find_one({"$and" : [{"sender": ObjectId(user_id)}, {"_id": ObjectId(message_id)}] })
-
-    message["_id"] = str(message["_id"])
-    message["sender"] = str(message["sender"])
-    message["receiver"] = str(message["receiver"])
-
-    return jsonify(message)
-
-
-@app.route("/message", methods=["POST"])
-def postMessage():
-    senderId = request.form["senderid"]
-    receiverId = request.form["receiverid"]
-    subject = request.form["subject"]
-    text = request.form["text"]
-
-    message = Message(sender=senderId, receiver=receiverId, subject=subject, message=text, sent=datetime.now())
-
-    return jsonify(message)
-
-# @app.route("/message/", methods=["DELETE"])
-# def deleteMessage():
-#     pass
-#
-@app.route("/user/<user_id>/message/<message_id>", methods=["PUT"])
-def putMessage(user_id, message_id):
-    message = request.form["message"]
-    senderId = request.form["senderid"]
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app = FlaskApp()
+    app.run(True)
--- resources/users_resource.py
+++ resources/users_resource.py
@@ -1,8 +1,7 @@
 __author__ = 'Modulus'
 #coding: utf-8
 
-from flask.ext.restful import Resource, Api, fields, marshal_with, reqparse
-
+from flask.ext.restful import Resource, marshal_with
 
 from models.user import User
 
