039d8d292b6593c775dbc79759736c81b71ce607
modulus <thejohnskauge@gmail.com>
2013-11-18 16:47:45 +0100

    User test now working. Still ned to fix unmarshalling of date/objecid

--- test/user_resource_test.py
+++ test/user_resource_test.py
@@ -1,13 +1,10 @@
-from unittest import TestCase
-from flask.ext.mongoengine import MongoEngine
-from mongoengine import connect
-from flask_app_new import app
-from flask_app_new import initApi, initDb
-from models.user import User
-
 __author__ = 'modulus'
 
-
+import json
+from unittest import TestCase
+from mongoengine import connect
+from flask_app_new import initApi, initDb, app
+from models.user import User
 
 
 class UserResouceTest(TestCase):
@@ -15,33 +12,56 @@ class UserResouceTest(TestCase):
     app.config.setdefault("MONGODB_DB", "demo_test")
     app.config["MONGODB_DB"] = "demo_test"
     app.config["MONGODB_HOST"] = "locahost"
-    # app.config["MONGODB_PORT"] = "28017
+    app.config["MONGODB_PORT"] = "27017"
     app.config["DEBUG"] = True
 
     connect(app.config["MONGODB_DB"])
     initDb()
     initApi()
+    client = app.test_client()
 
-# app.config.from_object("settings")
-    # @classmethod
-    # def tearDownClass(cls):
-    #     User.drop_collection()
-
+    @classmethod
+    def tearDownClass(cls):
+        User.drop_collection()
 
     def testDB(self):
         self.assertEquals("demo_test", app.config["MONGODB_DB"])
 
-    def test_post_user(self):
+    def testPostUser(self):
+
+        result = self.client.post("/user", data={"id": None, "first_name": "John", "last_name": "Skauge", "user_name": "John", "pass": "MyPassword"})
+
+        userDict = json.loads(result.data)
+        #Id should not leak out, this user dict is used in the ui
+        self.assertFalse(hasattr(userDict, "id"), "If this fails the user ID is leaked when created the user. "
+                                                  "This should not be returned when creating a user")
+        self.assertEquals("John", userDict["firstName"])
+        self.assertEquals("Skauge", userDict["lastName"])
+        self.assertTrue(userDict["password"])
+        self.assertTrue(userDict["created"])
+
+    def testDeleteUser(self):
+
+        result = self.client.post("/user", data={"id": None, "first_name": "John", "last_name": "Skauge", "user_name": "John", "pass": "MyPassword"})
+
+        userDict = json.loads(result.data)
+        #Id should not leak out, this user dict is used in the ui
+        self.assertFalse(hasattr(userDict, "id"), "If this fails the user ID is leaked when created the user. "
+                                                  "This should not be returned when creating a user")
+        self.assertEquals("John", userDict["firstName"])
+        self.assertEquals("Skauge", userDict["lastName"])
+        self.assertTrue(userDict["password"])
+        self.assertTrue(userDict["created"])
 
 
 
-        client = app.test_client()
 
-        result = client.post("/user", data={"id": None, "first_name": "John", "last_name": "Skauge", "user_name": "John", "pass": "MyPassword"})
-        self.assertTrue(result.data["id"])
-        self.assertEquals("John", result.data["firstName"])
-        self.assertEquals("Skauge", result.data["lastName"])
-        self.assertEquals("John", result.data["userName"])
+    def testRawUserSave(self):
+        user = User(firstName="John", lastName="Skauge", userName="John", password="MyPassword")
+        self.assertFalse(user.id)
+        user.save()
+        self.assertTrue(user.id)
+
 
 
 
